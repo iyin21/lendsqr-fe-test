89a63235a1eab671c0264ab4be176385c1594645
iyn21 <iyinoluwa.oluleke@gmail.com>
2022-10-12 22:39:21 +0100

    style: fix styling issues with search input

--- src/components/Layout/TopNav/topNav.tsx
+++ src/components/Layout/TopNav/topNav.tsx
@@ -1,5 +1,4 @@
 import Logo from '../../../assets/icons/lendsqr.svg';
-import Input from '../../Input/input';
 import Search from './assets/search.svg';
 import Bell from './assets/bell.svg';
 import Avatar from './assets/avatar.png';
@@ -20,15 +19,12 @@ const TopNav = ({ setOpenSideBar }: TopNavInterface) => {
                 <HiMenuAlt2 size={28} onClick={() => setOpenSideBar(true)} />
             </div>
             <div className={styles.input_container}>
-                <Input
-                    placeholder="Search for anything"
-                    className={styles.search}
-                    suffix={
-                        <div className={styles.search_img}>
-                            <img src={Search} alt="Search" />
-                        </div>
-                    }
-                />
+                <div className={styles.search}>
+                    <input placeholder="Search for anything" />
+                    <span className={styles.search_img}>
+                        <img src={Search} alt="Search" />
+                    </span>
+                </div>
             </div>
 
             <div className={styles.info_container}>
